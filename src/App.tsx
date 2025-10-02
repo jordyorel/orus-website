90af10a83fde3be10e7373f8e956e08d3c5f6144
Jordy Orel KONDA <jordyorel@protonmail.com>
2025-10-02 13:23:18 +0100

    Add interactive tutorial outline and sync with docs

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navigation from "./components/Navigation";
 import Home from "./pages/Home";
 import Playground from "./pages/Playground";
+import Tutorial from "./pages/Tutorial";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const AppContent = () => {
       {!isPlayground && <Navigation />}
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/tutorial" element={<Tutorial />} />
         <Route path="/play" element={<Playground />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
